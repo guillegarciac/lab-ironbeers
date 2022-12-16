351b64a11c5b213e668035fadca576510b8294e2
guille garcia <ggarciacardiel@gmail.com>
2022-12-16 12:18:01 +0100

    iteration 6 bonus - added beerpartial

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 hbs.registerHelper('json', function(context) {
